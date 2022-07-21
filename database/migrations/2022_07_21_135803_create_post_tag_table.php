c2f831110a82d8e48f3784c8113d7b7debb9895a
TrvMarco <trevisanello.marco@gmail.com>
2022-07-21 15:59:01 +0200

    creato relazione tag e post

--- database/migrations/2022_07_21_135803_create_post_tag_table.php
+++ database/migrations/2022_07_21_135803_create_post_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagsTable extends Migration
+class CreatePostTagTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('post_tag', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100)->unique();
-            $table->string('slug', 150);
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('post_tag');
     }
 }
