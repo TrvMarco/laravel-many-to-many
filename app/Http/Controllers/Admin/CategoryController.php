5d9f2bb9ab3e6e2a2f4d3d6ca6eaeeffc292e940
TrvMarco <trevisanello.marco@gmail.com>
2022-07-21 15:48:15 +0200

    first commit

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return view('admin.categories.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // validazione
+        $request->validate([
+            'name' => 'required|string|max:100'
+        ]);
+        // Inserimento
+        $data = $request->all(); 
+        $newCategory = new Category();
+        $newCategory->fill($data);
+        $newCategory->slug = Str::of($data['name'])->slug('-');
+        $newCategory->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return view('admin.categories.show', compact('category'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        return view('admin.categories.edit', compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'name' => 'required|string|max:100'
+        ]);
+
+        $category->update($request->all());
+
+        return redirect()->route('admin.categories.show', compact('category'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return redirect()->route('admin.categories.index');
+    }
+}
---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app');
+
+@section('content')
+    <div class="container">
+        <h1>Crea un post</h1>
+    </div>
+    <div class="container">
+        <form action="{{ route('admin.posts.store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+              <label for="title">Titolo</label>
+              <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title')}}" >
+              @error('title')
+              <div class="alert alert-danger">{{ $message }}</div>
+              @enderror
+            </div>
+            <div class="form-group">
+                <label for="content">Contenuto:</label>
+                <textarea class="form-control  @error('content') is-invalid @enderror" name="content" id="content" cols="30" rows="5" name="content">{{old('content')}}</textarea>
+                @error('content')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group">
+              <label for="category">Categoria post:</label>
+              <select class="form-control @error('category_id') is-invalid @enderror" id="category" name="category_id">
+                    <option value="">Seleziona</option>
+                @foreach ($categories as $category)
+                    <option value="{{$category->id}}" {{old('category_id') == $category->id ? 'selected' : '' }}>{{$category->name}}</option>
+                @endforeach
+              </select>
+              @error('category_id')
+                <div class="alert alert-danger">{{ $message }}</div>
+              @enderror
+            </div>
+            <div class="form-check mb-3">
+              <input type="checkbox" class="form-check-input @error('published') is-invalid @enderror" id="published" name="published" {{old('published') ? 'checked' : ''}}>
+              <label class="form-check-label" for="published">Pubblica il post</label>
+              @error('published')
+                <div class="alert alert-danger">{{ $message }}</div>
+              @enderror
+            </div>
+            <button type="submit" class="btn btn-primary">Crea Post</button>
+        </form>
+    </div>
+@endsection
